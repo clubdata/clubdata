ed5a196cb432808ccbab880cbddd9fea1c29c768
Fernando Marquardt <fernando.marquardt@gmail.com>
2017-04-07 16:52:33 -0300

    Formatting.

--- modules/admin/v_List.php
+++ modules/admin/v_List.php
@@ -14,9 +14,6 @@
  * @copyright Copyright (c) 2009, Franz Domes
  */
 
-/**
- *
- */
 require_once("include/dblist.class.php");
 
 /**
@@ -25,82 +22,79 @@ require_once("include/dblist.class.php");
  * @package Admin
  */
 class vList {
-    var $memberID;
-    var $db;
-    var $mlist;
-    var $feeID;
-    var $tableid;
-    var $pageNr;
-    var $smarty;
-    var $formsgeneration;
 
-    function vList($db, $table, $key,$smarty,$formsgeneration)
-    {
+    public $memberID;
+    public $db;
+    public $mlist;
+    public $feeID;
+    public $tableid;
+    public $pageNr;
+    public $smarty;
+    public $formsgeneration;
+
+    public function __construct($db, $table, $key, $smarty, $formsgeneration) {
         $this->db = $db;
         $this->table= $table;
         $this->smarty = $smarty;
         $this->formsgeneration = $formsgeneration;
 
         $this->tableid = "admin" . $this->table . "list";
-        $this->pageNr = getGlobVar("PageNr","::number::");
-        if (empty($this->pageNr) )
-        {
+        $this->pageNr = getGlobVar("PageNr", "::number::");
+
+        if (empty($this->pageNr)) {
             $this->pageNr = 1;
         }
 
         $sqlString = "SELECT * FROM `###_{$this->table}`";
 
-        if ( $key != -1 )
-        {
-            debug('M_ADMIN', "[vList] Reuse ($key) " . $this->tableid);
+        if ($key != -1) {
+            debug('M_ADMIN', "[vList] Reuse ({$key}) {$this->tableid}");
             $this->mlist = new DbList($db, $this->tableid);
+        } else {
+            debug('M_ADMIN', "[vList] Initialize View ({$key})");
+            $this->mlist = new DbList(
+                $db,
+                $this->tableid,
+                array(
+                    "changeFlg"     => getUserType(ADMINISTRATOR),
+                    "sql"           => $sqlString,
+                    "changeFlg"     => (($this->table == "Log") ? false : true),
+                    "selectRowsFlg" => true,
+                    "selectedRows"  => (getConfigEntry($this->db, "CheckedCheckboxes") ? "ALL" : "NONE"),
+                    //"maxRowsPerPage" => 20,
+                    "listLinks" => array(
+                        "Detail" => INDEX_PHP . "?mod=admin&view=Detail&Table=$this->table",
+                        "Edit"   => INDEX_PHP . "?mod=admin&view=Edit&Table=$this->table",
+                        "Delete" => INDEX_PHP . "?mod=admin&view=List&Action=DELETE&Table=$this->table"
+                    ),
+                    "linkParams" => "mod=admin&view=List&Table=$this->table"
+                )
+            );
         }
-        else
-        {
-            debug('M_ADMIN', "[vList] Initialize View ($key)");
-            $this->mlist = new DbList($db, $this->tableid,
-                                    array("changeFlg" => getUserType(ADMINISTRATOR),
-                                    "sql" => $sqlString,
-                                    "changeFlg" => (($this->table == "Log") ? FALSE : TRUE),
-                                    "selectRowsFlg" => TRUE,
-                                    "selectedRows" => (getConfigEntry($this->db, "CheckedCheckboxes") ? "ALL" : "NONE"),
-    //                                       "maxRowsPerPage" => 20,
-                                        "listLinks" => array ( "Detail" => INDEX_PHP . "?mod=admin&view=Detail&Table=$this->table",
-                                                            "Edit" => INDEX_PHP . "?mod=admin&view=Edit&Table=$this->table",
-                                                            "Delete" => INDEX_PHP . "?mod=admin&view=List&Action=DELETE&Table=$this->table",
-                                                            ),
-                                        "linkParams" => "mod=admin&view=List&Table=$this->table"));
-            }
 //             $tmp = $this->mlist->generateCondition();
 
-  		$tmpSort = getGlobVar('sort', '::text::');
+        $tmpSort = getGlobVar('sort', '::text::');
 
-		if ( !empty($tmpSort) )
-		{
-			$this->mlist->setConfig('sort', $tmpSort);
-		}
-        elseif ( $this->mlist->getConfig("sort") == "" )
-        {
+        if (!empty($tmpSort)) {
+            $this->mlist->setConfig('sort', $tmpSort);
+        } elseif ($this->mlist->getConfig("sort") == "") {
             $this->mlist->setConfig("sort", "id");
         }
 //             echo $this->mlist->generateSQL();
-
     }
 
-    function getSmartyTemplate()
-    {
+    public function getSmartyTemplate() {
 //         return CdBase::getSmartyTemplate();
         return 'admin/v_List.inc.tpl';
     }
 
-    function setSmartyValues()
-    {
+    public function setSmartyValues() {
         $this->formsgeneration->AddInput(array(
-                            "TYPE"=>"hidden",
-                            "NAME"=>'Table',
-                            "ID"=>'Table',
-                            "VALUE"=>$this->table,
-                            ));
+            "TYPE"=>"hidden",
+            "NAME"=>'Table',
+            "ID"=>'Table',
+            "VALUE"=>$this->table
+        ));
 
         $this->mlist->prepareRecordList($this->pageNr);
 //         debug_r('SMARTY', $this->mlist, "[v_List, setSmartyValues]: mlist");
@@ -108,19 +102,18 @@ class vList {
         $this->smarty->assign('Table', $this->table);
     }
 
-    function getLastPage()
-    {
+    public function getLastPage() {
         return $this->mlist->pagecount;
     }
+
     /**
-    * saves values passed via POST
-    * @return boolean true : save ok, false: error
-    */
-    function doAction($action)
-    {
+     * saves values passed via POST
+     * @return boolean true : save ok, false: error
+     */
+    public function doAction($action) {
         debug('ADMIN', "[v_List, doAction]: Action: $action");
-        switch ( $action )
-        {
+
+        switch ($action) {
             case 'SETCHECKED':
                 $id = getGlobVar('id', '::number::');
                 $newState = getGlobVar('newState', 'false|true');
@@ -128,16 +121,13 @@ class vList {
                 $this->mlist->setSelectedRows($id, ($newState == 'false' ? 0 : 1));
 
                 // Exit if ajax call is used
-                if ( getGlobVar('byAjax', 'true|false') == true )
-                {
-                	exit;
+                if (getGlobVar('byAjax', 'true|false') == true) {
+                    exit;
                 }
                 break;
-
             case 'SELECTALL':
                 $this->mlist->setAllSelectedRows(1);
                 break;
-
             case 'DESELECTALL':
                 $this->mlist->setAllSelectedRows(0);
                 break;
@@ -151,4 +141,3 @@ class vList {
     }
 */
 }
-?>
